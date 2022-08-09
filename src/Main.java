1fa720565e976f3aef3f668b9f42faab0d29ee90
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2022-08-10 01:58:21 +0200

    Started August 7th 2022. Not finished yet.
    Every code update will be after tests in external project.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+Here is clear code for Project of ConsoleMemoryGame.
+Every code update will be after tests in external project.
+Started August 7th 2022.
+Finished - not yet.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
